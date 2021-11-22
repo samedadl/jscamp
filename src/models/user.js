8eb8ba06536ed7e0e66421cc80f2f4329421cca9
samedadl <samedadl99@gmail.com>
2021-11-22 23:53:51 +0300

    22 kasım ders sonu

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,10 @@
 export default class User {
-    constructor(id, firstName, lastName, city) {//oluşturucu
+    constructor(id, firstName, lastName, city, age) {//oluşturucu
         // console.log("user oluştu")
         this.id = id
         this.firstName = firstName
         this.lastName = lastName
         this.city = city
+        this.age= age
     }
 }
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,22 +1,119 @@
-export default class UserService{
-    constructor(loggerService){
-        this.users = []//getCustomersFromDb()
+import { users } from "../data/users.js"
+import DataError from "../models/dataError.js"
+
+export default class UserService {
+    constructor(loggerService) {
+        //this.users = []//getCustomersFromDb()
+        this.employees = []
+        this.customers = []
+        this.errors = []
         this.loggerService = loggerService
     }
 
-    add(user){
-        this.users.push(user)
-        //console.log("kullanıcı eklendi " + user)
-        this.loggerService.log(user)
+    load() {
+        for (const user of users) {
+            //console.log(user)
+            switch (user.type) {
+                case "customer":
+                    if (!this.checkCustomerValidityForErrors(user)) {
+                        this.customers.push(user)
+                    }
+                    break;
+                case "employee":
+                    if (!this.checkEmployeeValidityForErrors(user)) {
+                        this.employees.push(user)
+                    }
+                    break;
+                default:
+                    this.errors.push(new DataError("Wrong user type", user))
+                    break;
+            }
+        }
     }
 
-    list(){
-        return this.users
+    // react te formik ve yup 
+    checkCustomerValidityForErrors(user) {
+        let requiredFields = "id firstName lastName age city".split(" ")
+        let hasErrors = false
+        //user["age"] = user.age
+        for (const field of requiredFields) {
+            if (!user[field]) {
+                hasErrors = true
+                this.errors.push(new DataError(`Validation problem ${field} is required`, user))
+            }
+        }
+        if (Number.isNaN(Number.parseInt(+user.age))) {
+            hasErrors = true
+            this.errors.push(new DataError(`Validation problem ${field} is not a number`, user))
+        }
+        return hasErrors
+    }
+
+
+    checkEmployeeValidityForErrors(user) {
+        let requiredFields = "id firstName lastName age city salary".split(" ")
+        let hasErrors = false
+        //user["age"] = user.age
+        for (const field of requiredFields) {
+            if (!user[field]) {
+                hasErrors = true
+                this.errors.push(new DataError(`Validation problem ${field} is required`, user))
+            }
+        }
+        if (Number.isNaN(Number.parseInt(user.age))) {
+            hasErrors = true
+            this.errors.push(new DataError(`Validation problem ${field} is not a number`, user))
+        }
+        return hasErrors
+    }
+
+    add(user) {
+        //this.users.push(user)
+        switch (users.type) {
+            case "customer":
+                if (!this.checkCustomerValidityForErrors(user)) {
+                    this.customers.push(user)
+                }
+                break;
+            case "employee":
+                if (!this.checkEmployeeValidityForErrors(user)) {
+                    this.employees.push(user)
+                }
+            default:
+                this.errors.push(new DataError("This user can not be added. Wrong user type", user))
+                break;
+        }
+        //console.log("kullanıcı eklendi " + user)
+       // this.loggerService.log(user)
+    }
+
+    list() {
+        //return this.users
         //console.log("kullanıcılar listelendi")
     }
 
-    getById(id){
-        return this.users.find(u=>u.id === id)
+    listCustomers() {
+        return this.customers
+    }
+
+    getById(id) {
+        //return this.users.find(u=>u.id === id)
         //console.log("kullanıcı detayı getirildi")
+    }
+
+    getCustomersById() {
+        return this.customers.find(u => u.id === id)
+    }
+
+    getCustomersSorted() {
+        return this.customers.sort((customer1, customer2) => {
+            if (customer1.firstName < customer2.firstName) {
+                return -1
+            } else if (customer1.firstName === customer2.firstName) {
+                return 0
+            } else {
+                return 1
+            }
+        })
     }
 }
